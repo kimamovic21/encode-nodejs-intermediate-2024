246a41fbf30d26d55f638d4ac6778c01fcadc184
Kerim Imamovic <imamovic.kerim@gmail.com>
2024-04-24 17:01:23 +0200

    izmjene u folderu 06-car-shop
    -u userController.js fajlu kreirane funkcije getUserById i deleteUser
    -u userRoutes.js fajlu kreirane nove dvije rute

--- 06-car-shop/controllers/userController.js
+++ 06-car-shop/controllers/userController.js
@@ -6,6 +6,22 @@ export const getAllUsers = async (req, res) => {
     res.status(200).send(users);
 };
 
+export const getUserById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        
+        const user = await User.findById(id);
+
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+
+        res.status(200).send(user);
+    } catch(error) {
+        res.status(500).send('Something went wrong');
+    }
+};
+
 export const updateUser = async (req, res) => {
     try {
         const { id } = req.params;
@@ -14,6 +30,18 @@ export const updateUser = async (req, res) => {
         const updatedUser = await User.findByIdAndUpdate(id, user);
 
         res.status(200).send('User updated successfully');
+    } catch(error) {
+        res.status(500).send('Something went wrong');
+    }
+};
+
+export const deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        
+        const deletedUser = await User.findByIdAndDelete(id);
+
+        res.status(200).send('User deleted successfully');
     } catch(error) {
         res.status(500).send('Something went wrong');
     }
--- 06-car-shop/routes/userRoutes.js
+++ 06-car-shop/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getAllUsers, updateUser } from '../controllers/userController.js';
+import { getAllUsers, getUserById, updateUser, deleteUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.get('/', getAllUsers);
+router.get('/:id', getUserById);
 router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 export default router;
